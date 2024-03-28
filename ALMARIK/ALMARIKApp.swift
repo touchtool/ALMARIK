948d841604eea3df9b6847e22d830943e0862cf8
Kittison Jackthreemongkol <78125325+touchtool@users.noreply.github.com>
2024-03-28 13:38:55 +0700

    Initial Commit

---
+++ ALMARIK/ALMARIKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALMARIKApp.swift
+//  ALMARIK
+//
+//  Created by ninew on 28/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ALMARIKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALMARIK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ALMARIK
+//
+//  Created by ninew on 28/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
